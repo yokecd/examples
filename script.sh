e3a4bc9677bb8ca9d7ca6310185f3ebc16ae84b7
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2025-01-11 10:29:04 -0500

    update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,21 @@
+set -eux
+
 # install yoke cli
 go install github.com/yokecd/yoke/cmd/yoke@latest
 
 # create a local cluster
-kind delete cluster && kind create cluster
+kind delete cluster
+
+kind create cluster --config - <<EOF
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+featureGates:
+  InformerResourceVersion: true
+  StorageVersionMigrator: true
+  APIServerIdentity: true
+runtimeConfig:
+  'storagemigration.k8s.io/v1alpha1': true
+EOF
 
 # install the atc
 yoke takeoff -wait 30s --namespace atc atc 'https://github.com/yokecd/yoke/releases/download/atc-installer%2Fv0.3.0/atc-installer.wasm.gz'
@@ -19,4 +32,25 @@ metadata:
 spec:
   image: nginx:latest
   replicas: 2
+  labels:
+    originalVersion: v1
+EOF
+
+# update the backendairway to use the v2 version.
+yoke takeoff -wait 30s backendairway "https://github.com/yokecd/examples/releases/download/latest/atc_backend_airway_v2.wasm.gz"
+
+# Now we can create v2 versions
+kubectl apply -f - <<EOF
+apiVersion: examples.com/v2
+kind: Backend
+metadata:
+  name: nginx-v2
+spec:
+  img: nginx:latest
+  replicas: 2
+  meta:
+    labels:
+      originalVersion: v1
+    annotations:
+      cool: 'yes'
 EOF
