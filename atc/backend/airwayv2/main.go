2145879f39240a84ccfa01bd356a67ea42bf127d
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2025-01-10 20:09:23 -0500

    added v2 backend with converter and airway_v2

---
+++ atc/backend/airwayv2/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"reflect"
+
+	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/yokecd/yoke/pkg/apis/airway/v1alpha1"
+	"github.com/yokecd/yoke/pkg/openapi"
+
+	v1 "github.com/yokecd/examples/atc/backend/v1"
+	v2 "github.com/yokecd/examples/atc/backend/v2"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	return json.NewEncoder(os.Stdout).Encode(v1alpha1.Airway{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "backends.examples.com",
+		},
+		Spec: v1alpha1.AirwaySpec{
+			WasmURLs: v1alpha1.WasmURLs{
+				Flight:    "https://github.com/yokecd/examples/releases/download/latest/atc_backend_v2_flight.wasm.gz",
+				Converter: "https://github.com/yokecd/examples/releases/download/latest/atc_backend_converter.wasm.gz",
+			},
+			Template: apiextv1.CustomResourceDefinitionSpec{
+				Group: "examples.com",
+				Names: apiextv1.CustomResourceDefinitionNames{
+					Plural:     "backends",
+					Singular:   "backend",
+					ShortNames: []string{"be"},
+					Kind:       "Backend",
+				},
+				Scope: apiextv1.NamespaceScoped,
+				Versions: []apiextv1.CustomResourceDefinitionVersion{
+					{
+						Name:    "v2",
+						Served:  true,
+						Storage: true,
+						Schema: &apiextv1.CustomResourceValidation{
+							OpenAPIV3Schema: openapi.SchemaFrom(reflect.TypeFor[v2.Backend]()),
+						},
+					},
+					{
+						Name:    "v1",
+						Served:  true,
+						Storage: false,
+						Schema: &apiextv1.CustomResourceValidation{
+							OpenAPIV3Schema: openapi.SchemaFrom(reflect.TypeFor[v1.Backend]()),
+						},
+					},
+				},
+			},
+		},
+	})
+}
