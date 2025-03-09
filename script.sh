a5611f36f533e15bcd41cdcf8d19290a4040bb73
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2025-03-09 15:46:00 -0400

    update script

--- script.sh
+++ script.sh
@@ -17,11 +17,14 @@ runtimeConfig:
   'storagemigration.k8s.io/v1alpha1': true
 EOF
 
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl patch -n kube-system deployment metrics-server --type=json -p '[{"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--kubelet-insecure-tls"}]'
+
 # install the atc
-yoke takeoff -wait 30s --create-namespace --namespace atc atc 'https://github.com/yokecd/yoke/releases/download/latest/atc-installer.wasm.gz'
+yoke takeoff -debug -wait 1m --create-namespace --namespace atc atc 'https://github.com/yokecd/yoke/releases/download/latest/atc-installer.wasm.gz'
 
 # install the yokcd/examples Backend-Airway
-yoke takeoff -wait 30s backendairway "https://github.com/yokecd/examples/releases/download/latest/atc_backend_airway.wasm.gz"
+yoke takeoff -debug -wait 1m backendairway "https://github.com/yokecd/examples/releases/download/latest/atc_backend_airway.wasm.gz"
 
 # You are done! You can now create Backends!
 kubectl apply -f - <<EOF
@@ -37,20 +40,20 @@ spec:
 EOF
 
 # update the backendairway to use the v2 version.
-yoke takeoff -wait 30s backendairway "https://github.com/yokecd/examples/releases/download/latest/atc_backend_airway_v2.wasm.gz"
+# yoke takeoff -wait 30s backendairway "https://github.com/yokecd/examples/releases/download/latest/atc_backend_airway_v2.wasm.gz"
 
 # Now we can create v2 versions
-kubectl apply -f - <<EOF
-apiVersion: examples.com/v2
-kind: Backend
-metadata:
-  name: nginx-v2
-spec:
-  img: nginx:latest
-  replicas: 2
-  meta:
-    labels:
-      originalVersion: v1
-    annotations:
-      cool: 'yes'
-EOF
+# kubectl apply -f - <<EOF
+# apiVersion: examples.com/v2
+# kind: Backend
+# metadata:
+#   name: nginx-v2
+# spec:
+#   img: nginx:latest
+#   replicas: 2
+#   meta:
+#     labels:
+#       originalVersion: v1
+#     annotations:
+#       cool: 'yes'
+# EOF
